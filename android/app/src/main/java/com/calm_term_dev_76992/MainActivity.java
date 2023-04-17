f249372dbc2f7fed6ef401531a09aed5beeab021
Crowdbotics <engineering@crowdbotics.com>
2023-04-17 01:36:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/calm_term_dev_76992/MainActivity.java
@@ -0,0 +1,15 @@
+package com.calm_term_dev_76992;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "calm_term_dev_76992";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calm-term-dev-76992",
+  url: "https://calm-term-dev-76992.botics.co",
+  api: "https://calm-term-dev-76992.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
